6f02cfcc2fd7e7fc9b4b7f065facad0daa894b5a
Gabriele Bilello <gabriele.bilello1@gmail.com>
2024-05-30 11:10:08 +0200

    feat: moved postgen scripts to cli

--- hooks/post_gen.dart
+++ hooks/post_gen.dart
@@ -1,17 +1,3 @@
-import 'dart:io';
-
 import 'package:mason/mason.dart';
 
-void run(HookContext context) async {
-  await runPnpm(
-    context: context
-  );
-}
-
-Future<void> runPnpm({
-  required HookContext context,
-}) async {
-  context.logger.info('📦 Running pnpm i');
-  await Process.run('pnpm', ['i']);
-  context.logger.success('📦 pnpm configured successfully 🚀');
-}
+void run(HookContext context) async {}
