306f5f859ddf943b4dae552ed4bced2706812057
erikamolino <erikamolino15@gmail.com>
2024-05-31 10:24:07 +0200

    fix: standard fonts usage

--- __brick__/src/app/layout.tsx
+++ __brick__/src/app/layout.tsx
@@ -1,9 +1,8 @@
 import "./globals.css";
 
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+import { inter } from "@/fonts";
 
 export const metadata: Metadata = {
   title: "Create Next App",
